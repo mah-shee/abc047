e33563053283ece7a47be93a27d9f8b2471ae57f
masa <masatoshi.sboard@gmail.com>
2020-09-18 08:20:10 +0900

    Done c

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -4,5 +4,20 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
+        s: Chars,
+    }
+
+    let mut count = 0;
+    // let mut prev_colour = s[0];
+    for i in 0..s.len() - 2 {
+        if s[i] != s[i + 1] {
+            count += 1;
+            // prev_colour = s[i + 1];
+        }
+    }
+    if s[0] == s[s.len() - 1] {
+        println!("{}", count);
+    } else {
+        println!("{}", count - 1);
     }
 }
--- src/bin/c.rs
+++ src/bin/c.rs
@@ -4,5 +4,16 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
+        s: Chars,
     }
+
+    let mut count = 0;
+    // let mut prev_colour = s[0];
+    for i in 0..s.len() - 1 {
+        if s[i] != s[i + 1] {
+            count += 1;
+            // prev_colour = s[i + 1];
+        }
+    }
+    println!("{}", count);
 }
